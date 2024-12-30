f8ea2f44e5c5e94775ebea69f491ce03596c3fc2
Jahanzaib <112988172+Jahanzaib0@users.noreply.github.com>
2024-12-30 14:43:51 +0500

    rm: minor things

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAT_wFTeZcgt1sBw7W2_AVbk4vhqyJygfs",
-  authDomain: "react-chat-app-dcf29.firebaseapp.com",
-  projectId: "react-chat-app-dcf29",
-  storageBucket: "react-chat-app-dcf29.appspot.com",
-  messagingSenderId: "79638019318",
-  appId: "1:79638019318:web:a95ab01d107d7c7a4cc0d7",
-  measurementId: "G-DKJCCSFSSE"
+ 
 };
 
 
